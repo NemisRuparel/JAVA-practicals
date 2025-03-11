4f75c081092ee794ac6564a3ec3596ce68f64aca
Nemis ‎ <nemisruparel07@gmail.com>
2025-03-11 23:45:21 +0530

    commited

---
+++ myPackage/PackageUse.java
@@ -0,0 +1,7 @@
+import myPackage.myPackage;
+public class PackageUse {
+    public static void main(String[] args) {
+        myPackage m1 = new myPackage();
+        m1.printhello();
+    }
+}
---
+++ myPackage/myPackage/myPackage.java
@@ -0,0 +1,11 @@
+package myPackage;
+public class myPackage{
+    public void printhello(){
+        System.out.println("Hello from package file");
+    }
+}
+// public class PackageDemo{
+//     public static void main(String[] s){
+        
+//     }
+// }
