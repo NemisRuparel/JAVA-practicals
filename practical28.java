b4ff20965ecbffb6220573f56df2d5cac7caacab
Nemis ‎ <nemisruparel07@gmail.com>
2025-03-20 22:02:22 +0530

    practical added

--- practical28.java
+++ practical28.java
@@ -1,5 +1,37 @@
+import java.util.*;
 public class practical28 {
     public static void main(String args[]){
-        
+        ArrayList<String> a = new ArrayList<String>();
+        LinkedList<String> l = new LinkedList<String>();
+
+        a.add("Sunday");
+        a.add("Monday");        
+        a.add("Tuesday");        
+        a.add("Wednesday");        
+        a.add("Thursday");        
+        a.add("Friday");        
+        a.add("Saturday");        
+
+        System.out.println("Arraylist :");
+        System.out.println(a);
+
+        l.add("January");
+        l.add("February");
+        l.add("March");
+        l.add("April");
+        l.add("May");
+        l.add("June");
+        l.add("July");
+        l.add("August");
+        l.add("September");
+        l.add("October");
+        l.add("November");
+        l.add("December");
+
+        System.out.println("\nLinkedList :");
+        System.out.println(l);
+
+
+
     }
 }
---
+++ practical29.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class practical29 {
+    public static void main(String s[]){
+    HashSet<String> hs = new HashSet<>();
+
+    hs.add("Yellow");
+    hs.add("Black");
+    hs.add("Red");
+    hs.add("Green");
+    hs.add("Blue");
+
+    
+    for(String i : hs){
+        System.out.println(i);
+    }
+}
+}
